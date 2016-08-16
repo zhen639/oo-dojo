0a4a03851eea74bd6384f0d0957926f88d482edf
Caizhen Liu <czliu@CNczliu.local>
2016-08-16 22:37:56 +0800

    using fetch for set arguments as nil or false

--- app/gear.rb
+++ app/gear.rb
@@ -1,8 +1,8 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
   def initialize(args)
-    @chainring = args[:chainring]
-    @cog = args[:cog]
+    @chainring = args.fetch(:chainring, 40)
+    @cog = args.fetch(:cog, 18)
     @wheel = args[:wheel]
   end
 end

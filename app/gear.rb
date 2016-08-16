b6bb1033974c6ce0e8399e595a783c3ab71b9c2e
Caizhen Liu <czliu@CNczliu.local>
2016-08-16 22:21:46 +0800

    using hash as arguments

--- app/gear.rb
+++ app/gear.rb
@@ -1,8 +1,8 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
-  def initialize(chainring, cog, wheel)
-    @chainring = chainring
-    @cog = cog
-    @wheel = wheel
+  def initialize(args)
+    @chainring = args[:chainring]
+    @cog = args[:cog]
+    @wheel = args[:wheel]
   end
 end

796adc1d971d45feadf1cacf2026571ecacaa887
Caizhen Liu <czliu@CNczliu.local>
2016-08-16 22:39:08 +0800

    isolate default arguments

--- app/gear.rb
+++ app/gear.rb
@@ -1,8 +1,13 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
   def initialize(args)
+    args = defaults.merge(args)
     @chainring = args.fetch(:chainring, 40)
     @cog = args.fetch(:cog, 18)
     @wheel = args[:wheel]
+  end
+
+  def defaults
+    {chainring: 40, cog: 18}
   end
 end
